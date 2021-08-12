53030a7cacbb72601066f5922c28e3d21d16aa3d
nyeinsu01 <nyeinsusu01@gmail.com>
2021-08-12 20:37:27 +0630

    quan

--- resources/views/backend/item/create.blade.php
+++ resources/views/backend/item/create.blade.php
@@ -7,7 +7,7 @@
         <div class="content-wrapper">
     <div class="card shadow mb-4">
       <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-mute d-inline-block">Subcategory Form</h6>
+        <h6 class="m-0 font-weight-bold text-mute d-inline-block">Item Form</h6>
 
         <a href="{{route('item.index')}}" class="btn btn-info float-right"><i class="fas fa-arrow-circle-left"></i></a>
       </div>
